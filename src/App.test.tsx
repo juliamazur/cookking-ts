9beedaffcf338d01c6f2e1030ee4107408cb15ce
juliamazur <julasia@gmail.com>
2018-10-30 20:12:55 +0100

    addToSchedule WIP

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,29 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+//import * as enzyme from "enzyme";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+// TODO addToSchedule is not seen probably by tslint
+//
+// describe("App", () => {
+//   it('has method addToSchedule', () => {
+//     const app = enzyme.shallow(<App/>);
+//     const fn = app.instance().addToSchedule;
+//     expect(typeof fn).toBe('function');
+//   });
+// });
+//
+// describe("App - addToSchedule", () => {
+//   it('throws when called without ID', () => {
+//     const app = enzyme.shallow(<App/>);
+//     const fn = app.instance().addToSchedule;
+//     expect(fn).toThrow();
+//   });
+//   // it('adds ID to the state.scheduleItems', () => {
+//   // });
+// });
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import * as React from 'react';
 import RecipeLibrary from './components/RecipeLibrary';
 import Schedule from './components/Schedule';
 
-class App extends React.Component {
+interface Props {}
+
+interface State {
+  scheduleItemsList: string[];
+}
+
+class App extends React.Component <Props, State> {
 
   recipeFixture = [
     {
@@ -16,13 +22,27 @@ class App extends React.Component {
     }
   ];
 
-  scheduleItemsFixture = ['rwerer','ewrwrfsd','sdfdf'];
+  state = {
+    scheduleItemsList: []
+  };
+
+  addToSchedule = () =>  {}
+
+  // addToSchedule = (recipeIdToAdd: string) =>  {
+  //   if(!recipeIdToAdd) {
+  //     throw new Error('No recipe ID given');
+  //   }
+  //
+  //   // TODO unable to push - why?
+  //   // let scheduleItemsList = this.state.scheduleItemsList.slice();
+  //   // this.setState({ scheduleItemsList: scheduleItemsList.push(recipeIdToAdd)});
+  // }
 
   public render() {
     return (
       <div>
-        <RecipeLibrary items={this.recipeFixture} addToSchedule={() => {console.log('add to schedule');}}/>
-        <Schedule  items={this.scheduleItemsFixture}/>
+        <RecipeLibrary items={this.recipeFixture} addToSchedule={this.addToSchedule}/>
+        <Schedule  items={this.state.scheduleItemsList}/>
       </div>
     );
   }
